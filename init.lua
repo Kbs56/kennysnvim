da24d4e78ce00319cad5b2719ca7ef620116d2fa
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-11-07 11:46:38 -0600

    add more opts

--- init.lua
+++ init.lua
@@ -53,6 +53,26 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "100"
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
