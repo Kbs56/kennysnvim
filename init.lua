1e4e7d2686e554c450cadfd8e665d436b5bea5e2
Kbs56 <kenny.sheldon99@gmail.com>
2023-11-08 21:17:30 -0600

    tab width change + proper lua caps

--- init.lua
+++ init.lua
@@ -51,9 +51,9 @@ require('lazy').setup({
 -- [[ Setting options ]]
 -- See `:help vim.o`
 -- NOTE: You can change these options as you wish!
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
--- lua/kennysheldon/plugins/none-ls.lua
+++ lua/kennysheldon/plugins/none-ls.lua
@@ -10,7 +10,7 @@ return {
         null_ls.builtins.formatting.golines,
       },
     })
-    on_attach = function(client, bufnr)
+    On_attach = function(client, bufnr)
       if client.supports_method("textDocument/formatting") then
         vim.api.nvim_clear_autocmds({
           group = augroup,
