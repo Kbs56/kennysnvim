c65af82fdc85386f242432a1e12806300f97a8a4
Kenneth Sheldon <kenneth.sheldon@aa.com>
2023-11-13 09:39:38 -0600

    New keymappings

--- lua/kennysheldon/plugins/telescope.lua
+++ lua/kennysheldon/plugins/telescope.lua
@@ -25,6 +25,7 @@ return {
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
     vim.keymap.set('n', '<leader>ps', builtin.find_files, { desc = '[P]roject [S]earch' })
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
     vim.keymap.set('n', '<leader>/', function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
--- lua/kennysheldon/plugins/terminal.lua
+++ lua/kennysheldon/plugins/terminal.lua
@@ -4,7 +4,7 @@ return {
     version = "*",
     opts = {
       vim.keymap.set('n', '<leader>t', vim.cmd.ToggleTerm),
-      vim.keymap.set('t', '<leader>t', vim.cmd.ToggleTerm),
+      vim.keymap.set('t', '<leader><leader>', vim.cmd.ToggleTerm),
       vim.keymap.set('t', '<esc>', [[<C-\><C-n>]]),
       vim.keymap.set('t', 'jk', [[<C-\><C-n>]]),
       vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]]),
