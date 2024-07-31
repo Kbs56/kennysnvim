9dba61b7c1ce32c558152807f6fe11946f1a4e49
Kenny Sheldon <kenny.sheldon99@gmail.com>
2024-07-30 21:28:58 -0500

    Neo tree config

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,5 +5,19 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
-  }
+  },
+  config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      enable_git_status = true,
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          never_show = {
+            ".git"
+          }
+        }
+      }
+    })
+  end
 }
